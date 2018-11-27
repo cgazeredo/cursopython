ede8b3be82a38c88f30eb32206215b664c93d6b0
cgazeredo <cg.azeredo@gmail.com>
2018-11-27 16:56:48 -0200

    aula 09 - manipulação de strings

---
+++ mundo01/aula-09_01-manipulacao-de-string.py
@@ -0,0 +1,11 @@
+frase = 'Curso em Video Python'
+
+print(frase[9]) #imprime décimo caracter
+print(frase[9:21]) #imprime do caracter 9 ate o 21 sem imprimir o 21
+print(frase[9:21:2]) #imprime do caracter 9 ate o 21 sem imprimir o 21 pulando de 2 em 2
+print(frase[:5]) #imprime do caracter 0 ate o 5 sem imprimir o 5
+print(frase[15:]) #imprime do caracter 15 ate o ultimo caracter
+print(frase[9::3])
+
+print(len(frase))
+print(frase.count('o'))
