162b4e5ca62230b191760774617c59333daec3fe
carlos.azeredo <cg.azeredo@gmail.com>
2018-12-03 17:13:00 -0200

    Exercicios 022 e 023

--- mundo01/aula-09_exercicios-022-manipulacao-string.py
+++ mundo01/aula-09_exercicios-022-manipulacao-string.py
@@ -1,14 +1,15 @@
-nome = input(' Digite um nome completo: ')
+nome = str(input(' Digite um nome completo: ')).strip()
 
 print('Todas Maisculas: ')
 print(nome.upper())
 print('Todas Minusculas:')
 print(nome.lower())
 print('Quantos caracteres sem contar espacos: ')
-sem_espaco = nome.strip()
-divido = sem_espaco.split()
+
+divido = nome.split()
 print(len(''.join(divido)))
 print('Numero de letras no primeiro nome: ')
+print(nome.find(' '))
 print(len(divido[0]))
 
 
---
+++ mundo01/aula-09_exercicios-023-digitos-de-um-numero.py
@@ -0,0 +1,16 @@
+numero = int(input('Digite um numero de 0 a 9999: '))
+
+u = numero//1 % 10
+d = numero//10 % 10
+c = numero//100 % 10
+m = numero//1000 % 10
+
+
+print('Analistando o numero {}...'.format(numero))
+print('Unidade: {}'.format(u))
+print('Dezena: {}'.format(d))
+print('Centena: {}'.format(c))
+print('Milhar: {}'.format(m))
+
+
+# nao consegui fazer sozinho
