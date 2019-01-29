35fee63b75fb0e2f17a4d6e9f9aacb69c9eaa929
cgazeredo <cg.azeredo@gmail.com>
2019-01-29 16:35:36 -0200

    aula 12 update do ex 037

--- mundo02/aula-12_exercicios-037-conversor-de-bases.py
+++ mundo02/aula-12_exercicios-037-conversor-de-bases.py
@@ -2,4 +2,7 @@ numero = int(input('Entre com um numero inteiro qualquer: '))
 base = int(input('Informe a base de conversão desejada: \n1-Binario\n2-Octal\n3-Hexadecimal'))
 if base == 1:
     print('O valor convertido de {} em Binario é: {}'.format(numero, numero))
+
+    while numero > 0:
+
 elif base == 2:
