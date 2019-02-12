a736b52ea552f67e74fe61ce4d287ee6e9c4c30e
cgazeredo <cg.azeredo@gmail.com>
2019-02-12 15:38:25 -0200

    Recommit do 42 deletado

---
+++ mundo02/aula-12_exercicios-042-tipo-triangulo.py
@@ -0,0 +1,17 @@
+a = float(input("Entre com o lado A do triangulo: "))
+b = float(input("Entre com o lado B do triangulo: "))
+c = float(input("Entre com o lado C do triangulo: "))
+
+if abs(b-c) < a < abs(b+c):
+    if abs(a-c) < b < abs(a+c):
+        if abs(a-b) < c < abs(a+b):
+            #  se as retas formam um triangulo, dizer que dipo de triangulo é
+            if a == b == c:
+                print('As retas formam um triangulo equilátero. ')
+            elif (a==b and a != c) or (a==c and a != b) or (b==c and b != a):
+                print('As retas formam um triangulo isoceles')
+            else:
+                print('As retas formam um triangulo escaleno.')
+
+else:
+    print("As retas a = {}, b = {} e c = {} não podem formar um triangulo".format(a, b, c))
