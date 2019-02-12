a3ec85c6a1ba1ae13ffa94b52180e3ec3a09af01
cgazeredo <cg.azeredo@gmail.com>
2019-02-12 18:05:27 -0200

    aula 13: introdução ao for

---
+++ mundo02/aula-13_01-estrura-repeticao-for.py
@@ -0,0 +1,9 @@
+
+#  no range o primeiro eh o inicio do contador, o segundo eh o fim, o terceiro eh como sera a iteraçao ex: 1 em 1,
+#  2 em 2, ou -1 em -1 ate 0
+
+for c in range(6, 0, -1):
+    print('OI')
+print('FIM')
+
+
